7d8bc2819c2f9272540131be1d29d42972705fe3
Schimmoellerm <schimmoellerm@gmail.com>
2021-02-25 17:59:41 -0500

    Created function to determine the color of the input boxes, also create a function to loop through these " git status ,

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,38 @@
-//Variables
+//Todays date and time
+
 var currentDay = $('#currentDay');
 
-//Todays date and time
 setInterval(function() {
     currentDay.text(moment().format('[Today is] MMMM Do, YYYY kk:mm'));
 }, 1000);
+
+/***************************************************************************/
+
+//Input color changer
+
+let currentTime = moment().format('H');
+console.log(currentTime);
+
+let timeArray = [9, 10, 11, 12,13,14,15,16,17];
+console.log(timeArray);
+
+function timeChecker(i) {
+    console.log(timeArray[i]);
+    console.log(currentTime);
+    console.log(parseInt(currentTime));
+    if (timeArray[i] < parseInt(currentTime)) {
+        console.log('grey')//set to grey
+    }else if (timeArray[i] > parseInt(currentTime)){
+        console.log('green')//set to green
+    }else if (timeArray[i] == parseInt(currentTime)){
+        console.log('red')//set to red
+    }
+}
+
+function timeLooper() {
+    for (i=0; i<timeArray.length; i++) {
+        timeChecker(i);
+    }
+}
+
+timeLooper();
