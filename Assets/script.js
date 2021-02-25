23ea412f7e7dc94814c892e38bcae8085ff547e6
Schimmoellerm <schimmoellerm@gmail.com>
2021-02-24 19:28:22 -0500

    Added time block, text inputs, and save buttons to index.html

--- Assets/script.js
+++ Assets/script.js
@@ -2,9 +2,6 @@
 var currentDay = $('#currentDay');
 
 //Todays date and time
-//var currentDay = moment().format("[Today is] dddd," + " MMMM"  + " DD," + " YYYY" + " kk:mm:ss");
-//$("#currentDay").text(currentDay);
-
 setInterval(function() {
     currentDay.text(moment().format('[Today is] MMMM Do, YYYY kk:mm'));
 }, 1000);
