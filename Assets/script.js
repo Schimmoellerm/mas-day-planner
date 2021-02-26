0a11a0fcf2ac00b8c0ca4dd12ebfefc289f88a23
Schimmoellerm <schimmoellerm@gmail.com>
2021-02-25 22:49:45 -0500

    Set a click event on each save button to save the corresponding input box value to the local storage

--- Assets/script.js
+++ Assets/script.js
@@ -39,8 +39,74 @@ timeLooper();
 
 /***************************************************************************/
 //Store inputs in local storage
-$('.saveBtn').on("click", function(){
-    console.log('Click Me Harder Daddy!');
-})
+$('#saveBtn1').on("click", function(){
+    console.log('Click1');
+    let timeSlot1 = $(this).attr('id');
+    let InputValue1 = $("#9").val()
+    localStorage.setItem(timeSlot1, InputValue1);
+    console.log(timeSlot1, InputValue1);
+});
 
+$('#saveBtn2').on("click", function(){
+    console.log('Click2');
+    let timeSlot2 = $(this).attr('id');
+    let InputValue2 = $("#10").val()
+    localStorage.setItem(timeSlot2, InputValue2);
+    console.log(timeSlot2, InputValue2);
+});
 
+$('#saveBtn3').on("click", function(){
+    console.log('Click2');
+    let timeSlot3 = $(this).attr('id');
+    let InputValue3 = $("#11").val()
+    localStorage.setItem(timeSlot3, InputValue3);
+    console.log(timeSlot3, InputValue3);
+});
+
+$('#saveBtn4').on("click", function(){
+    console.log('Click2');
+    let timeSlot4 = $(this).attr('id');
+    let InputValue4 = $("#12").val()
+    localStorage.setItem(timeSlot4, InputValue4);
+    console.log(timeSlot4, InputValue4);
+});
+
+$('#saveBtn5').on("click", function(){
+    console.log('Click2');
+    let timeSlot5 = $(this).attr('id');
+    let InputValue5 = $("#13").val()
+    localStorage.setItem(timeSlot5, InputValue5);
+    console.log(timeSlot5, InputValue5);
+});
+
+$('#saveBtn6').on("click", function(){
+    console.log('Click2');
+    let timeSlot6 = $(this).attr('id');
+    let InputValue6 = $("#14").val()
+    localStorage.setItem(timeSlot6, InputValue6);
+    console.log(timeSlot6, InputValue6);
+});
+
+$('#saveBtn7').on("click", function(){
+    console.log('Click2');
+    let timeSlot7 = $(this).attr('id');
+    let InputValue7 = $("#15").val()
+    localStorage.setItem(timeSlot7, InputValue7);
+    console.log(timeSlot7, InputValue7);
+});
+
+$('#saveBtn8').on("click", function(){
+    console.log('Click2');
+    let timeSlot8 = $(this).attr('id');
+    let InputValue8 = $("#16").val()
+    localStorage.setItem(timeSlot8, InputValue8);
+    console.log(timeSlot8, InputValue8);
+});
+
+$('#saveBtn9').on("click", function(){
+    console.log('Click2');
+    let timeSlot9 = $(this).attr('id');
+    let InputValue9 = $("#17").val()
+    localStorage.setItem(timeSlot9, InputValue9);
+    console.log(timeSlot9, InputValue9);
+});
